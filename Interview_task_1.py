90a803c6350bb8ae0a4174f95f815a9ff8742bb9
Eddiegeorgee <edward.george2712@gmail.com>
2024-05-24 20:19:35 +0300

    interview1

---
+++ Interview_task_1.py
@@ -0,0 +1,15 @@
+numbers = [12, 75, 150, 180, 145, 525, 50]
+
+num=numbers
+
+print(num) if numbers / 5 < 50 else print("error 404")
+
+# I tired to use short if
+
+# for number in numbers:
+#     if number > 500:
+#         break
+#     if number > 150:
+#         continue
+#     if number % 5 == 0:
+#         print(number)
