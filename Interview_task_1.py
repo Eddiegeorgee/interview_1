8376e3e56ffce8af32defbae6471b52044af56d5
Eddiegeorgee <edward.george2712@gmail.com>
2024-06-04 13:08:30 +0300

    add the task

--- Interview_task_1.py
+++ Interview_task_1.py
@@ -1,26 +1,17 @@
-numbers = [12, 75, 150, 180, 145, 525, 50]
+# numbers = [12, 75, 150, 180, 145, 525, 50]
 
-for number in numbers:
-    if number > 500:
-        break
-    if number > 150:
-        continue
-    if number % 5 == 0:
-        print(number)
+# for number in numbers:
+#     if number > 500:
+#         break
+#     if number > 150:
+#         continue
+#     if number % 5 == 0:
+#         print(number)
 
 #------------------------------------------------------------------------------------------------------------------------------------
 
-# #  Function to count the total number of digits in a number
-# def count_digits(number):
-#     count = 0
-#     while number != 0:
-#         number //= 10  # Integer division by 10 removes the last digit
-#         count += 1
-#     return count
-
-# # Example usage
-# number = 75869
-# print("The total number of digits in the number", number, "is:", count_digits(number))
+# write a program to count the total number of digits in a number using a while loop
+# for example the number is 75869, so the output should be 5
 
 
 #---------------------------------------------------------------------------------------------------------------------------------
@@ -56,7 +47,8 @@ for number in numbers:
 
 #------------------------------------------------------------------------------------------------------------------------------------
 #display Fibonacci series up to 10 terms
-# the Fibonacci sequence is a series of numbers. the next number is found by adding up the two numbers before it. the first two numbers are 0 and 1.
+# the Fibonacci sequence is a series of numbers. the next number is found by adding up the two numbers before it.
+# the first two numbers are 0 and 1.
 #for example, 0,1,1,2,3,5,8,13,21 the next number is this series above is 13+21=34
 
 
